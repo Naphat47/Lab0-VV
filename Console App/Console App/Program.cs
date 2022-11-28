10f456a9078d0b309eee43a9962dc512431f50e5
Naphat47 <119288598+Naphat47@users.noreply.github.com>
2022-11-28 16:16:59 +0700

    Add Lab0CV

---
+++ Console App/Console App/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("นาย ณภัทร สีหะวงค์");
+Console.WriteLine("653450508-7");
+Console.WriteLine("naphat.se@kkumail.com");
